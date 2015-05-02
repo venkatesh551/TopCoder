8b15bb69e8822b0a83cc56f541c7b190f6ebd88d
Venkateswarlu Pallamala <p.venkatesh551@gmail.com>
2015-05-02 22:50:03 +0530

    Organizing as Folders

---
+++ SRM 657/PolynomialRemainder.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.*;
+
+public class PolynomialRemainder {
+	public int findRoot(int a, int b, int c) {
+		return 0;
+	}
+
+// CUT begin
+	public static void main(String[] args){
+		System.err.println("PolynomialRemainder (1000 Points)");
+		System.err.println();
+		HashSet<Integer> cases = new HashSet<Integer>();
+        for (int i = 0; i < args.length; ++i) cases.add(Integer.parseInt(args[i]));
+        runTest(cases);
+	}
+
+	static void runTest(HashSet<Integer> caseSet) {
+	    int cases = 0, passed = 0;
+	    while (true) {
+	    	String label = Reader.nextLine();
+	    	if (label == null || !label.startsWith("--"))
+	    		break;
+
+            int a = Integer.parseInt(Reader.nextLine());
+            int b = Integer.parseInt(Reader.nextLine());
+            int c = Integer.parseInt(Reader.nextLine());
+            Reader.nextLine();
+            int __answer = Integer.parseInt(Reader.nextLine());
+
+            cases++;
+            if (caseSet.size() > 0 && !caseSet.contains(cases - 1))
+                continue;
+    		System.err.print(String.format("  Testcase #%d ... ", cases - 1));
+
+            if (doTest(a, b, c, __answer))
+                passed++;
+	    }
+	    if (caseSet.size() > 0) cases = caseSet.size();
+        System.err.println(String.format("%nPassed : %d/%d cases", passed, cases));
+
+        int T = (int)(System.currentTimeMillis() / 1000) - 1430572339;
+        double PT = T / 60.0, TT = 75.0;
+        System.err.println(String.format("Time   : %d minutes %d secs%nScore  : %.2f points", T / 60, T % 60, 1000 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))));
+	}
+
+	static boolean doTest(int a, int b, int c, int __expected) {
+		long startTime = System.currentTimeMillis();
+		Throwable exception = null;
+		PolynomialRemainder instance = new PolynomialRemainder();
+		int __result = 0;
+		try {
+			__result = instance.findRoot(a, b, c);
+		}
+		catch (Throwable e) { exception = e; }
+		double elapsed = (System.currentTimeMillis() - startTime) / 1000.0;
+
+		if (exception != null) {
+			System.err.println("RUNTIME ERROR!");
+			exception.printStackTrace();
+			return false;
+		}
+		else if (__result == __expected) {
+			System.err.println("PASSED! " + String.format("(%.2f seconds)", elapsed));
+			return true;
+		}
+		else {
+			System.err.println("FAILED! " + String.format("(%.2f seconds)", elapsed));
+			System.err.println("           Expected: " + __expected);
+			System.err.println("           Received: " + __result);
+			return false;
+		}
+	}
+
+	static class Reader {
+        private static final String dataFileName = "PolynomialRemainder.sample";
+	    private static BufferedReader reader;
+
+	    public static String nextLine() {
+	        try {
+                if (reader == null) {
+                    reader = new BufferedReader(new InputStreamReader(new FileInputStream(dataFileName)));
+                }
+                return reader.readLine();
+	        }
+	        catch (IOException e) {
+	            System.err.println("FATAL!! IOException");
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
+	        return "";
+	    }
+	}
+// CUT end
+}
